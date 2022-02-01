dc597473fdd4189a0d4e3ac66f851f108fe3401a
Jeff Gaboni <mrgabonijeff@gmail.com>
2022-02-02 02:26:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const bodyParser = require('body-parser')
 
 // Constants
 const port = process.env.PORT || 5000
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000'
+const FRONTEND_URL = process.env.FRONTEND_URL || 'https://gabonidev.github.io/'
 
 // Creating server
 const app = express()
